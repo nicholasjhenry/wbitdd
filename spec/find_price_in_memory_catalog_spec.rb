399fe4ff64e9e0142f11e025fe112ee3454aa872
Nicholas Henry <nicholas.henry@gmail.com>
2015-05-01 17:02:42 -0400

    We can find a price (in an InMemoryCatalog) if the price is there to be found.

---
+++ spec/find_price_in_memory_catalog_spec.rb
@@ -0,0 +1,22 @@
+require "price"
+
+describe "in-memory catalog" do
+  describe "find price" do
+    specify "product found" do
+      found_price = Price.cents(1250)
+      catalog = InMemoryCatalog.new("12345" => found_price)
+
+      expect(catalog.find_price("12345")).to eq(found_price)
+    end
+  end
+
+  class InMemoryCatalog
+    def initialize(prices_by_barcode)
+      @prices_by_barcode = prices_by_barcode
+    end
+
+    def find_price(barcode)
+      @prices_by_barcode[barcode]
+    end
+  end
+end

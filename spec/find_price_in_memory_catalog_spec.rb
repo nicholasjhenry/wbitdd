fb44038402febff7f29e17b7814882a6b5494fc5
Nicholas Henry <nicholas.henry@gmail.com>
2015-05-01 17:16:27 -0400

    Confirmed that InMemoryCatalog respects the semantics of Catalog.

--- spec/find_price_in_memory_catalog_spec.rb
+++ spec/find_price_in_memory_catalog_spec.rb
@@ -4,15 +4,22 @@ describe "in-memory catalog" do
   describe "find price" do
     specify "product found" do
       found_price = Price.cents(1250)
-      catalog = InMemoryCatalog.new("12345" => found_price)
+      catalog = catalog_with("12345", found_price)
 
       expect(catalog.find_price("12345")).to eq(found_price)
     end
 
     specify "product not found" do
-      catalog = InMemoryCatalog.new({})
-      expect(catalog.find_price("12345")).to be_nil
+      expect(catalog_without("12345").find_price("12345")).to be_nil
     end
+  end
+
+  def catalog_with(barcode, price)
+    InMemoryCatalog.new(barcode => price)
+  end
+
+  def catalog_without(barcode_to_avoid)
+    InMemoryCatalog.new({})
   end
 
   class InMemoryCatalog
